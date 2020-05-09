c1373bb68e47fc1df0edeadcd89cff0ba711cf11
kevinbourdal <kevin.bourda@outlook.com>
2020-05-08 21:20:03 -0300

    fix Head

--- rematar-frontend/src/App.js
+++ rematar-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LoginForm from './components/LoginFormComponent'
 import Contact from './components/ContactComponent'
+import Head from './components/Head';
+
 class App extends Component {
     render() {
         return (
             <div>
-               <Contact />
+               <Head />
             </div>
         );
     }
--- rematar-frontend/src/components/Head.js
+++ rematar-frontend/src/components/Head.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import { Badge } from 'reactstrap';
+
 import React, { useState, Component } from 'react';
 import {
   Collapse,
