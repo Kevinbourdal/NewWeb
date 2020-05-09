304f5ce869bed5bcc677fa130f1d62ed574ab389
cristiancontrera95 <cristiancontrera95@gmail.com>
2020-05-08 22:02:59 -0300

    Add routing

--- rematar-frontend/src/App.js
+++ rematar-frontend/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import LoginForm from './components/LoginFormComponent'
-import Contact from './components/ContactComponent'
+import { Container } from 'reactstrap';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import LoginForm from './components/LoginFormComponent';
+import Contact from './components/ContactComponent';
 import Head from './components/Head';
 
 class App extends Component {
     render() {
         return (
             <div>
-               <Head />
+                <Head />
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/login">
+                           <LoginForm />
+                        </Route>
+                        <Route path="/contact">
+                            <Contact />
+                        </Route>
+                   </Switch>
+               </BrowserRouter>
             </div>
         );
     }
