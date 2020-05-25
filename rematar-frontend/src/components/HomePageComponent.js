4c4ce0652b2824589c83c6e7c92b4f45f25d5403
cristiancontrera95 <cristiancontrera95@gmail.com>
2020-05-25 19:01:27 -0300

    fix nav categories

--- rematar-frontend/src/components/HomePageComponent.js
+++ rematar-frontend/src/components/HomePageComponent.js
@@ -48,90 +48,87 @@ const HomePage = (props) => {
 
         return (
          <div>
-           <div className="p-0 m-0" style={{ height: '600px'}}>
-              <div className="container-fluid no-padding h-25 p-0 m-0 ">
+            <div className="p-0 m-0" style={{ height: '600px'}}>
+               <div className="container-fluid no-padding h-25 p-0 m-0 ">
 
-                 <div  style={{ height: '70vh' }} className="p-0 m-0">
-                    <style>
-                       {`.custom-tag {
-                                      max-width: 100%;
-                                      height: 600px;
-                                      background: black;
-                                    }`}
-                    </style>
-                    <Card style={{ width: "100%"}}>
+                  <div  style={{ height: '70vh' }} className="p-0 m-0">
+                     <style>
+                        {`.custom-tag {
+                              max-width: 100%;
+                              height: 600px;
+                              background: black;
+                        }`}
+                     </style>
+                     <Card style={{ width: "100%"}}>
+                        <Carousel
+                           slide={false}
+                           activeIndex={activeIndex}
+                           next={next}
+                           previous={previous}
+                        >
+                           <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+                           {slides}
+                           <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                           <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+                        </Carousel>
 
-                       <Carousel
-                          slide={false}
-                          activeIndex={activeIndex}
-                          next={next}
-                          previous={previous}>
-                          <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-                          {slides}
-                          <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-                          <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-                       </Carousel>
+                        <CardImgOverlay className="text-white justify-content-center text-center align-text-bottom h-75 mt-5">
+                           <div className="">
+                              <Row className="my-5">
+                                 <h3 className="w-100 text-white-100 mt-4">10 Lotes nuevos este mes</h3>
+                              </Row>
+                              <Row className="my-5">
+                                 <h1 className="w-100" ><b className="">  Remates Calamuchita  </b></h1>
+                              </Row>
 
-                       <CardImgOverlay className="text-white justify-content-center text-center align-text-bottom h-75 mt-5">
-                          <div className="">
-                             <Row className="my-5">
-                                <h3 className="w-100 text-white-100 mt-4">10 Lotes nuevos este mes</h3>
-                             </Row>
-                             <Row className="my-5">
-                                <h1 className="w-100" ><b className="">  Remates Calamuchita  </b></h1>
-                             </Row>
-
-                             <Row className="my-5">
-                                <MDBContainer  className=" mr-5">
-                                   <MDBFormInline className="md-form ml-5">
-                                      <input className="form-control mr-sm-1 text-white"
-                                             style={{width:"80%"}}
-                                             type="text"
-                                             placeholder="Buscar"
-                                             aria-label="Search" />
-
-                                      <MDBBtn gradient="aqua" rounded size="sm" type="submit">
-                                         <b>Buscar</b>
-                                      </MDBBtn>
-                                   </MDBFormInline>
-                                </MDBContainer>
-                             </Row>
-                             <Row className="my-5">
-                                <h2 className="w-100">Encontra las mejores inversion en los remates de terrenos, casas, Campos, etc..</h2>
-                             </Row>
-
-                          </div>
-                       </CardImgOverlay>
-                    </Card>
-                    <MDBCol >
-                       <MDBRow className="justify-content-center">
-                          <NavFiltro/>
-                       </MDBRow>
-                    </MDBCol>
-                 </div>
-              </div>
-           </div>
-
-
-
-                <div className="mt-5 container-fluid"  >
-                    <div className="ml-5">
-                      <h2><b>{ itemslist.length }  Lotes  disponibles: </b></h2>
-                    </div>
-                    <MDBRow >
-                    <MDBCol className="col-md-3 mt-3 "  >
-                       <section className="rounded-lg card "  style={{backgroundColor:'#bdbdbd ', width:"100%" , height: '100%'}} >
-                       <FiltrosForHome />
-                       </section>
-                     </MDBCol>
-                     <MDBCol className="ml-2 mr-4 ">
-                      <CardGallery itemslist={itemslist}/>
-                    </MDBCol>
-                </MDBRow>
+                              <Row className="my-5">
+                                 <MDBContainer  className=" mr-5">
+                                    <MDBFormInline className="md-form ml-5">
+                                       <input
+                                          className="form-control mr-sm-1 text-white"
+                                          style={{width:"80%"}}
+                                          type="text"
+                                          placeholder="Buscar"
+                                          aria-label="Search"
+                                       />
+                                       <MDBBtn gradient="aqua" rounded size="sm" type="submit">
+                                          <b>Buscar</b>
+                                       </MDBBtn>
+                                    </MDBFormInline>
+                                 </MDBContainer>
+                              </Row>
+                              <Row className="my-5">
+                                 <h2 className="w-100">Encontra las mejores inversion en los remates de terrenos, casas, Campos, etc..</h2>
+                              </Row>
+                           </div>
+                        </CardImgOverlay>
+                     </Card>
+                  </div>
+               </div>
             </div>
-
+            <div>
+               <MDBCol className="">
+                  <MDBRow className="justify-content-center">
+                     <NavFiltro/>
+                  </MDBRow>
+               </MDBCol>
             </div>
-
+            <div className="mt-5 container-fluid"  >
+               <div className="ml-5">
+                  <h2><b>{ itemslist.length }  Lotes  disponibles: </b></h2>
+               </div>
+               <MDBRow >
+                  <MDBCol className="col-md-3 mt-3 "  >
+                     <section className="rounded-lg card "  style={{backgroundColor:'#bdbdbd ', width:"100%" , height: '100%'}} >
+                        <FiltrosForHome />
+                     </section>
+                  </MDBCol>
+                  <MDBCol className="ml-2 mr-4 ">
+                     <CardGallery itemslist={itemslist}/>
+                 </MDBCol>
+               </MDBRow>
+            </div>
+         </div>
         );
 }
 
--- rematar-frontend/src/components/NavFiltro.js
+++ rematar-frontend/src/components/NavFiltro.js
@@ -4,23 +4,22 @@ import {MDBInput, MDBNav, MDBNavItem, MDBNavLink, MDBIcon} from "mdbreact";
 
 export default  () => (
     <BrowserRouter>
-        <MDBNav  className="md-col-12 justify-center text-center elegant-color-dark " style={{  width:"100%"}}>
+        <MDBNav  className="md-col-12 justify-center text-center elegant-color-dark" style={{ width:"100%" }}>
             <MDBNavItem className="col-md-3">
                 <MDBNavLink  className="" style={{color:"white"}} to="#!">
-                    <i className="fas fa-tractor" style={{color: "#1DA1F2"}}></i> Terrenos</MDBNavLink>
+                    <i className="fas fa-tractor" style={{color: "#1DA1F2"}}></i> Terrenos </MDBNavLink>
             </MDBNavItem>
             <MDBNavItem className="col-md-3">
                 <MDBNavLink className="" style={{color:"white"}} to="#!">
-                    <i className="fas fa-home" style={{color: "#1DA1F2"}}></i> Casas</MDBNavLink>
+                    <i className="fas fa-home" style={{color: "#1DA1F2"}}></i> Casas </MDBNavLink>
             </MDBNavItem>
             <MDBNavItem className="col-md-3">
                 <MDBNavLink  className="" style={{color:"white"}} to="#!">
-                    <i className="fas fa-car" style={{color: "#1DA1F2"}}></i> Autos</MDBNavLink>
+                    <i className="fas fa-car" style={{color: "#1DA1F2"}}></i> Autos </MDBNavLink>
             </MDBNavItem >
             <MDBNavItem className="col-md-3">
                 <MDBNavLink className="" style={{color:"white"}} to="#!">
-                    <i className="fas fa-motorcycle" style={{color: "#1DA1F2"}}></i> Motos
-                </MDBNavLink>
+                    <i className="fas fa-motorcycle" style={{color: "#1DA1F2"}}></i> Motos </MDBNavLink>
             </MDBNavItem>
         </MDBNav>
 
