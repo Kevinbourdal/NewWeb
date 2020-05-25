35a81d3bd5a818cabe0fa4fd487f7c5e32177240
cristiancontrera95 <cristiancontrera95@gmail.com>
2020-05-25 14:14:21 -0300

    Add docker for prod and dev

---
+++ rematar-frontend/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Author: Cristian Contrera
+
+DOCKER_NAME="frontend"
+DOCKER_PROD_NAME="frontend-prod"
+
+if [ "$1" == 'prod' ];
+then
+  echo "Start server on production mode"
+
+  exist_docker=$(docker images | grep DOCKER_PROD_NAME | wc -l)
+  if [ $(( $is_docker )) == 0 ];
+  then
+    echo "Docker image not found. Building from file.."
+    docker build -t $DOCKER_PROD_NAME -f Dockerfile_prod .
+  fi
+
+  echo "Image ready, starting container.."
+  docker run -it -p 80:3000 $DOCKER_PROD_NAME
+  exit 0;
+fi
+
+exist_docker=$(docker images | grep $DOCKER_NAME | wc -l)
+if [ $(( $is_docker )) == 0 ];
+then
+  echo "Docker image not found. Building from file.."
+  docker build -t $DOCKER_NAME .
+fi
+
+echo "Image ready, starting container.."
+docker run -it -p 3000:3000 DOCKER_NAME
+
+
