dc79d9a73189898d3968c56459b755322493c9be
cristiancontrera95 <cristiancontrera95@gmail.com>
2020-05-29 21:50:54 -0300

    >

--- rematar-backend/configdb.sh
+++ rematar-backend/configdb.sh
@@ -1,104 +1,40 @@
 ## Correr en la maquina donde se ejecutara el docker
+# ========================================================================================
 
+## MySQL
+# ===============================================================
 # download mysql:8
 docker pull mysql:8
 
-# Executar mysql with name and root password
-docker run --name=mysql-subastas -e MYSQL_ROOT_PASSWORD=fernetconcoca2020! -d mysql:8
-
-# connect to docker
-docker exec -ti mysql-subastas bash
-
-
-### Comandos a ejecutar dentro del docker
-# ========================================================================================
-# open mysql console
-mysql -u root -p
-# create database
-CREATE DATABASE subastasenweb;
-# create an new user
-CREATE USER 'api_account'@'localhost' IDENTIFIED BY 'fernetconcoca2020';
-# Set all privileges to user created
-GRANT ALL PRIVILEGES ON *.* TO 'api_account'@'localhost';
-FLUSH PRIVILEGES;
-QUIT
-
-exit
-
-## start docker again (si apagaste la compu)
-#docker container start mysql-subastas
+# Creamos un container que ejecutara mysql
+docker run -d --name=mysql-subastas -e MYSQL_ROOT_PASSWORD=fernetconcoca2020! -v $PWD/db:/docker-entrypoint-initdb.d/:ro mysql:8
 
 ### Get mysql docker ip address
 docker inspect mysql-subastas | grep "IPAddress"
-
 # Con esta IP, el puerto: 3306 y el nombre de la base de datos ("subastasenweb") te podes conectar
 # seteando en src/config.py los campos
 
 
+## start docker again (si apagaste la compu)
+#docker container start mysql-subastas
 
 
-### Conectart database con models.py
-# ========================================================================================
-# ejecutar la primera vez
-rm -r migrations/  # solo si existe una carpera migrations/
-python migrate.py db init
-
-### Ejecutar cada vez que modifique models.py (Para actualizar los cambios hechos, ej nuevas columnas, o tablas )
-python migrate.py db migrate
-python migrate.py db upgrade
+## Instalar driver para conectarse a mysql desde python
+# ===============================================================
+sudo apt-get install default-libmysqlclient-dev python3-dev -y
 
 
-
-
-# Instalar driver para conectarse a mysql desde python
-# ========================================================================================
-
-
-# Opcion 1   ##### Con docker
-docker build -t backend-remates .
-docker run -d --name=backend-remates --network="host" -p 5000:5000 -v $PWD/src:/app/. backend-remates
-
-docker inspect backend-remates | grep "IPAddress"
-# Con esta IP, el puerto: 5000 te podes conectar
-# desde el frontend
-
-
-### Entrar al docker con consola
-# docker exec -it  backend-remates /bin/bash
-
-###FIN
+## Sincronizar tablas desde python a mysql
+# ===============================================================
+# ejecutar desde la carpeta NewWeb/rematar-backend/
+./migrate.sh
 
 
 
+## Ejecutar la api
+# ===============================================================
+# Activamos el virtualenv
+source venv/bin/activate
 
-
-# Opcion 2 ##### Instalar en el sistema
-
-### Librerias requeridas para conectarse a mysql a travez de la API
-
-# install driver and connector
-sudo apt install odbcinst1debian2 libodbc1 unixodbc-dev python3-dev default-libmysqlclient-dev python3-mysqldb -y
-
-# activar virtualenv si no tiene pip instalado en el sistema
-#. venv/bin/activate
-pip install mysqlclient
-
-
-### Descargar el driver a utilizar por la api para conectarse a MySql
-# download driver url:
-# https://dev.mysql.com/downloads/connector/odbc/
-
-# Ubuntu 20 LTS:
-$ wget https://dev.mysql.com/get/Downloads/Connector-ODBC/8.0/mysql-connector-odbc_8.0.20-1ubuntu20.04_amd64.deb
-sudo dpkg -i mysql-connector-odbc_8.0.20-1ubuntu20.04_amd64.deb
-
-<<<<<<< HEAD
-###FIN
-=======
-### Ejcutar con virtualenv activado:
-
-rm -r migrations/
-python migrate.py db init
-python migrate db migate
-python migrate.py db upgrade
->>>>>>> 127502e23053859cb19aff286cea0cf67f7322be
+cd src/
+python run.py
--- rematar-backend/src/config.py
+++ rematar-backend/src/config.py
@@ -7,23 +7,18 @@ DEBUG = False
 WTF_CSRF_ENABLED = True
 PROD = False
 
-DATABASE = {
+DB = {
     'driver': '{MySQL ODBC 8.0 Driver}',
-    'server': 'db',
-    'port': '5001',
+    'server': '172.17.0.2',
+    'port': '3306',
     'database': 'subastasenweb',
-    'username': 'api_account',
+    'username': 'root',
     'password': 'fernetconcoca2020!',
 }
 
-params = urllib.parse.quote_plus(f'DRIVER={DATABASE["driver"]};' +
-                                 f'SERVER={DATABASE["server"]};' +
-                                 f'DATABASE={DATABASE["database"]};' +
-                                 f'UID={DATABASE["username"]};' +
-                                 f'PWD={DATABASE["password"]}')
 
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = f'mysql://{DATABASE["username"]}:{DATABASE["password"]}@{DATABASE["server"]}:{DATABASE["port"]}/{DATABASE["database"]}'
+SQLALCHEMY_DATABASE_URI = f'mysql://{DB["username"]}:{DB["password"]}@{DB["server"]}:{DB["port"]}/{DB["database"]}'
 
 SQLALCHEMY_POOL_RECYCLE = 3600
